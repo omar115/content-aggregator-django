6a42582e27a3cd33c748f28aa3968fa70431051c
omar115 <omarhasan115@gmail.com>
2022-06-08 14:06:06 +0600

    updated-readme file

--- main.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-feed = feedparser.parse('https://realpython.com/podcasts/rpp/feed')
-podcast_title = feed.channel.title
-print(podcast_title)
-podcast_image = feed.channel.image['href']
-print(podcast_image)
